db4321b75018bb6208502d36171cbb52f760d96f
shinooooo <bjys1290150@gn.iwasaki.ac.jp>
2020-04-17 16:45:57 +0900

    finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -7,12 +7,23 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       post signup_path, params: {user: { name: "",
                               email: "user@invalid",
                               password: "foo",
-                              password_confirmation: "bar"
-       }}
+                              password_confirmation: "bar" }}
     end
     assert_template 'users/new'
     assert_select 'div#error_explanation'
     assert_select 'div.alert'
     assert_select 'form[action="/signup"]'
+  end
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: {user: {name: "Example User",
+                              email: "user@exmple.com",
+                              password: "password",
+                              password_confirmation: "password" }}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    asser_not flash.empty?
   end
 end
