e85a4ff46e6424dfb82e24d9787842e50bb9bbd8
shinooooo <bjys1290150@gn.iwasaki.ac.jp>
2020-04-30 01:37:14 +0900

    fin chapter 11

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,12 +27,20 @@ class User < ApplicationRecord
 
   def authenticated?(attribute, token)
     digest = send("#{attribute}_digest")
-    return false if remember_digest.nil?
+    return false if digest.nil?
     BCrypt::Password.new(digest).is_password?(token)
   end
   
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  def activate
+    update_columns(activated: true, activated_at: Time.zone.now)
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
   end
 
   private
